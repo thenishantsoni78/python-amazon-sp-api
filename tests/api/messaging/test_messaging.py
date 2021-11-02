0fe60717d4bdeb66e8ae65eced9b3a36be1cf5d8
Michael <info@saleweaver.com>
2021-11-02 18:35:57 +0100

    check error

--- tests/api/messaging/test_messaging.py
+++ tests/api/messaging/test_messaging.py
@@ -1,3 +1,7 @@
 from sp_api.api.messaging.messaging import Messaging
 from sp_api.base import Marketplaces
 
+
+def test_create_negative_feedback_removal_request():
+    res = Messaging().create_negative_feedback_removal('123-1234567-1234567')
+    print(res)
